d3e7dd4eb9b8581e101320acedf301109770d7d1
Ugmar <marchukovanton6@gmail.com>
2025-10-05 22:09:36 +0300

    test

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
-	// "dz1/task_1"
-	// "dz1/task_2"
+	"dz1/task_1"
+	"dz1/task_2"
 	"dz1/task_3"
 )
 
 func main(){
-	slice := []int{1, 2, 3}
 
-		// When
+	slice := []int{1, 2, 3}
 	err := task3.ScaleSlice(&slice, 3)
 	fmt.Println(slice, err)
+
+	err = task2.FindCommonWords("../task_2/files/result.txt", "../task_2/files/t1", "../task_2/files/t2", "../task_2/files/t3")
+	fmt.Println(err)
+
+	a, b, rc := task1.FilterCommomDigits(1234, 498124)
+	fmt.Println(a, b, rc)
+
 }
